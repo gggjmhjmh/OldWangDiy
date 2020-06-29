7faf80a6b5398ab0546b3bb429940f6096dd66ef
wangshuai <gggjmhjmh@163.com>
2020-06-29 17:11:38 +0800

    .

--- app/src/test/java/com/app/yf/myapplication/ExampleUnitTest.java
+++ app/src/test/java/com/app/yf/myapplication/ExampleUnitTest.java
@@ -10,15 +10,12 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
 
-        System.out.println(333 / 0.0195);
-
-
-        System.out.println(Integer.toHexString((int) (255 * 0.65)));
-
+        System.out.println("16进制: " + Integer.toHexString((int) (255 * 0.65)));
 
     }
 }
