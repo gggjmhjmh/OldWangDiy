4f01534f78f97b27bd1b4307b4161d060333ace7
wangshuai <gggjmhjmh@163.com>
2020-07-08 16:11:10 +0800

    lib_location: MyLocationManager定位管理工具的封装;

--- lib_location/src/main/java/com/oldwang/lib_location/MyLocationManager.java
+++ lib_location/src/main/java/com/oldwang/lib_location/MyLocationManager.java
@@ -26,45 +26,42 @@ import java.util.List;
 public abstract class MyLocationManager {
 
     public static int LOCATION_SERVICE_CODE = 1;
+    public static int LOCATION_PERMISSION_CODE = 2;
 
     abstract void start(Context context);
 
     abstract void stop();
 
+    /**
+     * 设置定位成功的回调监听
+     *
+     * @param listener
+     */
     abstract void setListener(LocationCallback listener);
 
     public interface LocationCallback {
         void onLocationSucceed(LocationInfo info);
+    }
 
+
+    /**
+     * 是否已打开定位权限和定位服务的回调
+     */
+    public interface IsOpenedCallback {
+        void opened(); //已打开定位权限和定位服务
+
+        void notOpenPermission(); //未打开定位权限
+
+        void notOpenService(); //未打开定位服务
     }
 
     /**
-     * 是否启用定位服务
+     * 检查定位权限和定位服务
+     * 没有打开就会跳转到系统设置页面去打开
      *
-     * @param mContext
-     * @return
+     * @param mContext Activity
      */
-    public boolean isOpenLocationService(Context mContext) {
-        LocationManager mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
-        //是否启用定位服务
-//        if (!mLocationManager.isProviderEnabled(LocationManager.KEY_PROVIDER_ENABLED)) {
-        if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
-            return true;
-        }
-        return false;
-    }
-
-    /**
-     * 去开启定位服务
-     *
-     * @param mContext
-     */
-    public void toOpenLocationService(Context mContext) {
-        //去开启定位设置页面
-        ((Activity) mContext).startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), LOCATION_SERVICE_CODE);
-    }
-
-    public void onViewClicked(Context mContext) {
+    public static void checkPermissionAndService(Context mContext, IsOpenedCallback openedCallback) {
 
        /* try {
             // 如果没有打开定位
@@ -77,32 +74,66 @@ public abstract class MyLocationManager {
         AndPermission.with(mContext).permission(Permission.ACCESS_FINE_LOCATION).onGranted(new Action<List<String>>() {
             @Override
             public void onAction(List<String> permissions) {
-                // TODO what to do.
                 Log.i("custome", "timer onGranted");
 
                 if (isOpenLocationService(mContext)) {
-//                    finish();
+                    if (openedCallback != null)
+                        openedCallback.opened();
                 } else {
                     Log.i("onRefresh: ", "onRefresh: 未启用定位服务");
-                    toOpenLocationService(mContext);//去开启定位设置页面
+
+                    if (openedCallback != null)
+                        openedCallback.notOpenService();
+
+//                    toOpenLocationService(mContext);//去开启定位设置页面
                 }
             }
         }).onDenied(new Action<List<String>>() {
             @Override
             public void onAction(List<String> permissions) {
-                // TODO what to do
                 Log.i("custome", "timer onDenied");
-                toSystemSetPage(mContext);//去设置权限
+
+                if (openedCallback != null)
+                    openedCallback.notOpenPermission();
+
+//                toSystemSetPage(mContext);//去设置权限
             }
         }).start();
 
-
-//        //去开启定位设置页面
-//        startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), 1);
     }
 
-    //跳转到系统该应用的设置页面
-    private void toSystemSetPage(Context mContext) {
+    /**
+     * 是否启用定位服务
+     *
+     * @param mContext Activity
+     * @return
+     */
+    public static boolean isOpenLocationService(Context mContext) {
+        LocationManager mLocationManager = (LocationManager) mContext.getSystemService(Context.LOCATION_SERVICE);
+        //是否启用定位服务
+//        if (!mLocationManager.isProviderEnabled(LocationManager.KEY_PROVIDER_ENABLED)) {
+        if (mLocationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) || mLocationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)) {
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     * 去开启定位服务
+     *
+     * @param mContext Activity
+     */
+    public static void toOpenLocationService(Context mContext) {
+        //去开启定位设置页面
+        ((Activity) mContext).startActivityForResult(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS), LOCATION_SERVICE_CODE);
+    }
+
+    /**
+     * 跳转到系统该应用的设置页面
+     *
+     * @param mContext Activity
+     */
+    public static void toSystemSetPage(Context mContext) {
         //去设置权限
         Intent intent = new Intent();
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
@@ -118,8 +149,7 @@ public abstract class MyLocationManager {
             intent.setAction("android.settings.APPLICATION_DETAILS_SETTINGS");
             intent.setData(Uri.fromParts("package", mContext.getPackageName(), null));
         }
-        ((Activity) mContext).startActivityForResult(intent, 2);
-        ((Activity) mContext).startActivity(intent);
+        ((Activity) mContext).startActivityForResult(intent, LOCATION_PERMISSION_CODE);
     }
 
 
